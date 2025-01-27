a98c04cdb5b3849427f7252077d09421adbd2f90
Askar <a.khaimuldin@pin-up.tech>
2025-01-27 08:34:48 +0500

    edit Student

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import models.Student;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello amigo!");
 
-        Student student = new Student(7, "Bake");
+        Student student = new Student(7, "Bake", true);
 
         System.out.println(student);
     }
--- src/models/Student.java
+++ src/models/Student.java
@@ -12,4 +12,5 @@ import lombok.Setter;
 public class Student {
     private int id;
     private String name;
+    private boolean gender;
 }
