3c73032912290134fffb9eb8a803601f25ddac29
Askar <a.khaimuldin@pin-up.tech>
2025-01-27 08:32:31 +0500

    edit Student

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import models.Student;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hola!");
 
-        Student student = new Student(7, "Bake");
+        Student student = new Student(7, "Bake", "Bakebayev");
 
         System.out.println(student);
     }
