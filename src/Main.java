305a0d398693fc4a172f38f957355930f0a1cce0
Askar <a.khaimuldin@pin-up.tech>
2025-01-27 08:30:00 +0500

    created Student and Main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import models.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Student student = new Student(7, "Bake");
+
+        System.out.println(student);
+    }
+}
